354f9fb1a5621a5f58cdad611217ea4dbf4325f0
JJJZ25 <144878743+JJJZ25@users.noreply.github.com>
2023-12-27 15:39:05 +0800

    Add files via upload

--- group3 import dataset.R
+++ group3 import dataset.R
@@ -1,13 +1,11 @@
 library(readr)
 md <- jsonlite::fromJSON("https://vipmbr.cpc.com.tw/openData/electricmotoData")
 
-## 整合表格內每一個項目
 name <- md$站名
 number <- md$郵遞區號
 address <- md$地址
 service <- md$服務類型
 code <- md$站代號
-
 charger <- data.frame(
   name = md$站名,
   number = md$郵遞區號,
@@ -20,15 +18,6 @@ md$服務類型 |>
   table()
 
 # 分析服務時段（是否為24小時服務）----
-## old ----
-{
-  pick <- md$站名 == "金山站"
-  md$提供服務時段[pick]
-  class(md$提供服務時段)
-  table(md$提供服務時段)
-  
-  md$提供服務時段 |> head()
-}
 ## new ----
 md |>
   dplyr::filter(
@@ -38,16 +27,17 @@ md |>
 View(md24hrs)
 
 # 分析充電站類型服務時段（24小時服務站點數量）
-
 table(md24hrs$服務類型)
 
-# 利用電話區域碼分析地區數量（北、中、南）
-sort(number, decreasing = T)
-phone = data.frame(md$站名, md$電話)
-phone[[1, 2]]
-x = phone[[1, 2]]
-#-----------
-call <- substr(md$電話, 2, 4)
-table(call)
+# 利用電話區域碼分析地區數量（北、中、南）----
+md$電話 |>
+  # 有"("或")"就斷開
+  stringr::str_split("[()]") -> split_phone
+zip = vector("character", length(split_phone))
+for(i in seq_along(zip)){
+  zip[[i]] <- split_phone[[i]][[2]]
+}
+table(zip) |>
+  sort(decreasing=T)
 
 
